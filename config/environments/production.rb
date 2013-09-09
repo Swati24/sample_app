a32cd5ac876f92b2e7b3bf23f2a31a6fc2cbc663
Swati Aggarwal <swati.aggarwal2412@gmail.com>
2013-09-10 04:18:12 +0530

    sendgrid added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,21 @@ SampleApp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { :host => 'sampleapp24.herokuapp.com' }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
